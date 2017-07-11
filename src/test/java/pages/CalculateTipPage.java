9cf93b072e94c1beb4af55261b8aee7efc22e9bd
@AK <anik.sqa@gmail.com>
2017-07-11 15:41:16 +0100

    AK updated the test with IOS

--- src/test/java/pages/CalculateTipPage.java
+++ src/test/java/pages/CalculateTipPage.java
@@ -4,6 +4,7 @@ import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.MobileElement;
 import io.appium.java_client.pagefactory.AndroidFindBy;
 import io.appium.java_client.pagefactory.AppiumFieldDecorator;
+import io.appium.java_client.pagefactory.iOSFindBy;
 import org.openqa.selenium.support.PageFactory;
 
 
@@ -15,18 +16,22 @@ public class CalculateTipPage extends PageBaseClass {
         PageFactory.initElements(new AppiumFieldDecorator(driver),this);
     }
 
+    @iOSFindBy(className = "XCUIElementTypeTextField")
     @AndroidFindBy(id = "org.traeg.fastip:id/totalAmtTextView")
     MobileElement textBillAmountElement;
 
+    @iOSFindBy(accessibility = "Calculate Tip")
     @AndroidFindBy(id="org.traeg.fastip:id/calcTipButton")
     MobileElement calculateTripElement;
 
     @AndroidFindBy(id="org.traeg.fastip:id/tipPctTextView")
     MobileElement tipPercentageText;
 
+    @iOSFindBy(accessibility = "$60.00")
     @AndroidFindBy(id="org.traeg.fastip:id/tipAmtTextView")
     MobileElement tipAmountText;
 
+    @iOSFindBy(accessibility = "$460.00")
     @AndroidFindBy(id="org.traeg.fastip:id/totalAmtTextView")
     MobileElement totalAmountText;
 
--- src/test/java/testarea/CalculateTipJourney.java
+++ src/test/java/testarea/CalculateTipJourney.java
@@ -13,7 +13,6 @@ public class CalculateTipJourney extends TestBaseClass {
     {
         new CalculateTipPage(driver).calculateTip();
 
-        assertEquals("15.0%",new CalculateTipPage(driver).getTipPerc());
         assertEquals("$60.00",new CalculateTipPage(driver).getTipAmount());
         assertEquals("$460.00",new CalculateTipPage(driver).getTotalAmount());
 
